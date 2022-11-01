bb2b9b5ba43b857c20dce32f5a6ced600023180c
Roberto Rojo <rrojo.va@gmail.com>
2022-11-01 18:03:00 +0100

    Initial Commit

---
+++ ParallelAsyncAwait/ParallelAsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallelAsyncAwaitApp.swift
+//  ParallelAsyncAwait
+//
+//  Created by Roberto Rojo Sahuquillo on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ParallelAsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

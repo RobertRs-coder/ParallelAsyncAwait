a48ea8149736574b8ceda3922721cf71c5c3194e
Roberto Rojo <rrojo.va@gmail.com>
2022-11-01 18:16:15 +0100

    Insert ViewModel inside ContentView & call loadImages method

--- ParallelAsyncAwait/ContentView.swift
+++ ParallelAsyncAwait/ContentView.swift
@@ -8,12 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject private var viewModel = TestViewModel()
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+                .task {
+                    //Call viewModel method
+                    viewModel.loadImages()
+                }
         }
         .padding()
     }
